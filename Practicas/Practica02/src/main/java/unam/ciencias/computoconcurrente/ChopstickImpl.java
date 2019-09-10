c3835068afbe1adb6b8d57c84686fc5f191b67ab
DamianRi <dams_rive@ciencias.unam.mx>
2019-09-10 17:12:11 -0500

    Subiendo Chopstick

--- Practicas/Practica02/src/main/java/unam/ciencias/computoconcurrente/ChopstickImpl.java
+++ Practicas/Practica02/src/main/java/unam/ciencias/computoconcurrente/ChopstickImpl.java
@@ -1,18 +1,36 @@
 package unam.ciencias.computoconcurrente;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ChopstickImpl implements Chopstick {
     private int id;
+    private AtomicInteger aTimes;
+    private AtomicBoolean aBoolean; 
+    private int timesTaken; 
+    private boolean taken;
 
     public ChopstickImpl(int id) {
         this.id = id;
+        this.aTimes = new AtomicInteger(0);
+        this.timesTaken = aTimes.get();
+        this.aBoolean = new AtomicBoolean(false);
+        this.taken = aBoolean.get();
     }
 
     @Override
-    public void take() {
-    }
+    public void take(){
+        this.timesTaken = aTimes.incrementAndGet();
+        while(this.taken);
+        System.out.println("Tomando Chopstick ");
+        this.aBoolean.set(true);
+        this.taken = aBoolean.get();
+    }   
 
     @Override
     public void release() {
+        this.aBoolean.set(false);
+        this.taken = aBoolean.get();
     }
 
     @Override
@@ -24,6 +42,7 @@ public class ChopstickImpl implements Chopstick {
     @Override
     public int getTimesTaken() {
         // TODO: Adjust with the right value
-        return 0;
+        //return 0;
+        return timesTaken;
     }
 }
